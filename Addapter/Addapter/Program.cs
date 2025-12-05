d47a9712f393463ddfd494389d9f852056e7e4d8
NikitaShchegolev <i@nnshchegolev.ru>
2025-12-05 11:20:19 +0300

    ++

--- Addapter/Addapter/Program.cs
+++ Addapter/Addapter/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace Addapter
+﻿using Addapter.ExemplesClass;
+
+namespace Addapter
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            CardInfo cardInfo = new CardInfo() 
+            {
+                Id = Guid.NewGuid(),
+                StarDate = DateTime.UtcNow,
+                message = new Message() { Content = "Привет!"}
+            };
+
+            var cardManager = new CardManager();
+            cardManager.Issue(cardInfo);
+            Console.WriteLine(cardManager.Issue(cardInfo));
+            Console.ReadKey();
+            
         }
     }
 }
