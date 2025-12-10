a5841a488a4b6778ebffd1fb29586ae291c9569b
NikitaShchegolev <i@nnshchegolev.ru>
2025-12-10 13:34:19 +0300

    +++

--- Addapter/Decorator/Messages/EmailNotificationDecorator.cs
+++ Addapter/Decorator/Messages/EmailNotificationDecorator.cs
@@ -2,40 +2,44 @@
 using Decorator.ExemplesInterface;
 
 namespace Decorator.Messages
+{
+    /// <summary>
+    /// Декоратор для отправки уведомлений по электронной почте.
+    /// </summary>
+    internal class EmailNotificationDecorator : BaseDecorator
     {
+        #region Пояснения
+        //1. public EmailNotificationDecorator(...) - это объявление конструктора класса EmailNotificationDecorator.Конструктор имеет такое же имя, как и класс.
+
+        //2. (INotyficationService notyficationService) - параметр конструктора.Он принимает объект, реализующий интерфейс INotyficationService.Это позволяет передать в декоратор существующий сервис уведомлений, который он будет "оборачивать".
+
+        //3. : base(notyficationService) - вызов конструктора базового класса. Это означает, что параметр notyficationService передается в конструктор родительского класса (вероятно, абстрактного класса декоратора).
+
+        //4. Пустое тело { } - в данном случае конструктор не выполняет никакой дополнительной логики, кроме передачи параметра в базовый класс.
+
+        //Такой паттерн используется в паттерне "Декоратор", где:
+
+        //У вас есть базовый интерфейс (INotyficationService)
+        //Конкретные реализации этого интерфейса
+        //Декораторы, которые "оборачивают" базовый сервис, добавляя дополнительную функциональность
+
+        #endregion
+
         /// <summary>
-        /// Декоратор для отправки уведомлений по электронной почте.
+        /// Инициализирует новый экземпляр класса EmailNotificationDecorator.
         /// </summary>
-        internal class EmailNotificationDecorator : BaseDecorator
+        /// <param name="notyficationService">Сервис уведомлений, который будет декорирован.</param>
+        public EmailNotificationDecorator(INotyficationService notyficationService) : base(notyficationService) { }
+
+        /// <summary>
+        /// Реализует виртулальный класс . Отправляет уведомление по электронной почте
+        /// через базовый BaseDecorator.
+        /// </summary>
+        public override void Notify()
         {
-            //1. public EmailNotificationDecorator(...) - это объявление конструктора класса EmailNotificationDecorator.Конструктор имеет такое же имя, как и класс.
+            base.Notify();
 
-            //2. (INotyficationService notyficationService) - параметр конструктора.Он принимает объект, реализующий интерфейс INotyficationService.Это позволяет передать в декоратор существующий сервис уведомлений, который он будет "оборачивать".
-
-            //3. : base(notyficationService) - вызов конструктора базового класса. Это означает, что параметр notyficationService передается в конструктор родительского класса (вероятно, абстрактного класса декоратора).
-
-            //4. Пустое тело { } - в данном случае конструктор не выполняет никакой дополнительной логики, кроме передачи параметра в базовый класс.
-
-            //Такой паттерн используется в паттерне "Декоратор", где:
-
-            //У вас есть базовый интерфейс (INotyficationService)
-            //Конкретные реализации этого интерфейса
-            //Декораторы, которые "оборачивают" базовый сервис, добавляя дополнительную функциональность
-
-            /// <summary>
-            /// Инициализирует новый экземпляр класса EmailNotificationDecorator.
-            /// </summary>
-            /// <param name="notyficationService">Сервис уведомлений, который будет декорирован.</param>
-            public EmailNotificationDecorator(INotyficationService notyficationService) : base(notyficationService) { }
-
-            /// <summary>
-            /// Отправляет уведомление по электронной почте.
-            /// </summary>
-            public override void Notify()
-            {
-                base.Notify();
-
-                Console.WriteLine("Выполнен - EmailNotificationDecorator");
-            }
+            Console.WriteLine("Выполнен - EmailNotificationDecorator");
         }
     }
+}
--- Addapter/Decorator/Messages/FaceBookNotificationDecorator.cs
+++ Addapter/Decorator/Messages/FaceBookNotificationDecorator.cs
@@ -8,31 +8,34 @@ namespace Decorator.Messages
         /// </summary>
         public class FaceBookNotificationDecorator : BaseDecorator
         {
-            //1. public EmailNotificationDecorator(...) - это объявление конструктора класса
-            //EmailNotificationDecorator.Конструктор имеет такое же имя, как и класс.
+        #region Пояснения
+        //1. public EmailNotificationDecorator(...) - это объявление конструктора класса
+        //EmailNotificationDecorator.Конструктор имеет такое же имя, как и класс.
 
-            //2. (INotyficationService notyficationService) - параметр конструктора.
-            //Он принимает объект, реализующий интерфейс INotyficationService.
-            //Это позволяет передать в декоратор существующий сервис уведомлений, который он будет "оборачивать".
+        //2. (INotyficationService notyficationService) - параметр конструктора.
+        //Он принимает объект, реализующий интерфейс INotyficationService.
+        //Это позволяет передать в декоратор существующий сервис уведомлений, который он будет "оборачивать".
 
-            //3. : base(notyficationService) - вызов конструктора базового класса.
-            //Это означает, что параметр notyficationService передается в конструктор
-            //родительского класса (вероятно, абстрактного класса декоратора).
+        //3. : base(notyficationService) - вызов конструктора базового класса.
+        //Это означает, что параметр notyficationService передается в конструктор
+        //родительского класса (вероятно, абстрактного класса декоратора).
 
-            //4. Пустое тело { } - в данном случае конструктор не выполняет никакой
-            //дополнительной логики, кроме передачи параметра в базовый класс.
+        //4. Пустое тело { } - в данном случае конструктор не выполняет никакой
+        //дополнительной логики, кроме передачи параметра в базовый класс.
 
-            //Такой паттерн используется в паттерне "Декоратор", где:
+        //Такой паттерн используется в паттерне "Декоратор", где:
 
-            //У вас есть базовый интерфейс (INotyficationService)
-            //Конкретные реализации этого интерфейса
-            //Декораторы, которые "оборачивают" базовый сервис, добавляя дополнительную функциональность
+        //У вас есть базовый интерфейс (INotyficationService)
+        //Конкретные реализации этого интерфейса
+        //Декораторы, которые "оборачивают" базовый сервис, добавляя дополнительную функциональность
 
-            /// <summary>
-            /// Инициализирует новый экземпляр класса FaceBookNotificationDecorator.
-            /// </summary>
-            /// <param name="notyficationService">Сервис уведомлений, который будет декорирован.</param>
-            public FaceBookNotificationDecorator(INotyficationService notyficationService) : base(notyficationService) { }
+        #endregion
+
+        /// <summary>
+        /// Инициализирует новый экземпляр класса FaceBookNotificationDecorator.
+        /// </summary>
+        /// <param name="notyficationService">Сервис уведомлений, который будет декорирован.</param>
+        public FaceBookNotificationDecorator(INotyficationService notyficationService) : base(notyficationService) { }
 
             /// <summary>
             /// Отправляет уведомление через Facebook.
